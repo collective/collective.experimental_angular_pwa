5d59e2028b75e073de21aa5334d53f591f8f7ad7
envy7 <noel.varghese.22@gmail.com>
2017-05-23 20:13:20 +0530

    refresh cache functionality added

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -3,6 +3,7 @@ import { Http, RequestOptions, Headers } from '@angular/http';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ResourceService } from '@plone/restapi-angular';
 import { Observable } from 'rxjs/Rx';
+import { Storage } from '@ionic/storage';
 
 @IonicPage()
 @Component({
@@ -12,28 +13,55 @@ import { Observable } from 'rxjs/Rx';
 export class SettingsPage {
   readOffline: boolean = false;
   queryUrls: any = [];
+  //number of milliseconds before querying server for refreshing cache
+  refreshAfter: number = 60*60*1000;
+  lastRefreshAt: number = 0;
+  refreshCache;
+  storage = new Storage(localStorage);
 
   constructor(public navCtrl: NavController, 
               public navParams: NavParams,
               private resService: ResourceService,
               private http: Http) {
+      this.storage.get("downloadOffline").then( (val) => {
+        this.readOffline = val;
+        this.toggleDownload();
+      })              
   }
 
   toggleDownload() {
     if(this.readOffline) {
+        this.storage.set("downloadOffline", "true");
+        //call downloadOffline at fixed intervals to refresh cache
         this.downloadOffline();
+        this.refreshCache = setInterval( () => {
+            this.downloadOffline();
+        }, this.refreshAfter)
+    }
+    else {
+        this.storage.set("downloadOffline", "false");
+        clearInterval(this.refreshCache);
     }
   }
 
-  downloadOffline() {
+  downloadOffline() { 
     let emptyQuery = {
-      "SearchableText" : ''
+      "SearchableText"  : '',
+      "metadata_fields" : 'modified'
     }
     this.resService.find(emptyQuery, "/").subscribe( (data) => {
       console.log(data);
-      this.queryUrls = data.items.map( (item) => {
-        return item['@id'];
-      });
+      this.queryUrls = data.items
+        .filter( (item) => {
+          if(this.lastRefreshAt == 0)
+            return item;
+          else {
+            return new Date(item.modified).getTime() > this.lastRefreshAt;
+          }  
+        }) 
+        .map( (item) => {
+          return item['@id']; //return only urls of all items
+        })
       for(let url in this.queryUrls) {
         let queryUrl = this.queryUrls[url];
         this.cacheUrl(queryUrl).subscribe((data) => {
@@ -44,6 +72,7 @@ export class SettingsPage {
           console.log(data);
         }) 
       }
+      this.lastRefreshAt = new Date().getTime();
     })
   }   
 
