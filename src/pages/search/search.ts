c64232f55fb4c78c8890eb405eedf70dba937ace
envy7 <noel.varghese.22@gmail.com>
2017-06-13 21:57:39 +0530

    Handled empty search and added sorting to results

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -9,23 +9,41 @@ import { ResourceService } from '@plone/restapi-angular';
 export class SearchPage {
 
   query: string = '';
-  searchResults: any[];
+  searchResults: any[] = [];
+  sortBy: string = 'relevance';
+  queryObj: any;
+  noResult: boolean = false;
 
   constructor(public navCtrl: NavController, 
               public navParams: NavParams,
               private resService: ResourceService) {
   }
 
-  search(queryText) {
-    console.log(queryText);
-    let query = {
-      "SearchableText" : queryText
+  search(query, empty) {
+    //check if user wants to perform an empty query
+    if(!empty && query === '') {
+      this.noResult = false;
+      return;
     }
-    this.resService.find(query, "/").subscribe( (data) => {
-      // this.searchResults = data.items.filter(function(item){
-      //   return item['@type'] !== 'Collection';
-      // });
+    this.queryObj = {
+      "SearchableText" : query
+    }
+    if(this.sortBy === 'date') {
+      this.queryObj.sort_on = "Date";
+      this.queryObj.sort_order = "reverse";
+    }
+    else if(this.sortBy === 'alphabetically') {
+      this.queryObj.sort_on = "sortable_title";
+      this.queryObj.sort_order = "reverse";
+    }
+    this.resService.find(this.queryObj, "/").subscribe( (data) => {
       this.searchResults = data.items;
+      if(this.searchResults.length == 0) {
+        this.noResult = true;
+      }
+      else {
+        this.noResult = false;
+      }
       console.log(this.searchResults);
     })
   }
