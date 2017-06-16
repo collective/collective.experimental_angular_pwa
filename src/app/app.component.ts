33fda74a72c877bd04754baa3d1065093f156fbf
envy7 <noel.varghese.22@gmail.com>
2017-06-16 17:53:37 +0530

    Implemented loader between pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component, ViewChild, Injectable } from '@angular/core';
-import { Platform, Nav} from 'ionic-angular';
+import { Platform, Nav, LoadingController} from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { Traverser, Marker } from 'angular-traversal';
-import { PloneViews } from '@plone/restapi-angular';
+import { PloneViews, APIService } from '@plone/restapi-angular';
 import { EventComponent, FolderComponent, PlonesiteComponent,
          DocumentComponent, ImageComponent, LinkComponent,
          FileComponent, NewsitemComponent } from '../views/views';
@@ -29,7 +29,9 @@ export class MyApp {
               statusBar: StatusBar, 
               splashScreen: SplashScreen,
               private views: PloneViews,
-              private traverser: Traverser) {
+              private traverser: Traverser,
+              private api: APIService,
+              private loadingCtrl: LoadingController) {
     
     this.views.initialize();
     this.traverser.addView('view', 'Event', EventComponent);
@@ -46,12 +48,44 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+
+      this.api.loading.subscribe((value) => {
+        if(!value)
+          this.apiLoadsDone++;
+        this.handleLoading(value); 
+      }) 
+
     });
 
   }
 
-    openSettings() {
-      this.nav.push(SettingsPage);
+  loading;
+  //currently two page loads being performed, resource and breadcrumbs
+  apiLoadsNeeded = 2;
+  apiLoadsDone = 0;
+  isLoading: boolean = false;
+
+  openSettings() {
+    this.nav.push(SettingsPage);
+  }
+
+  handleLoading(value) {
+    if(value) {
+      if(this.apiLoadsDone == 0) {
+        this.loading = this.loadingCtrl.create({
+          content: "Loading..."
+        });
+        this.loading.present();
+      }
     }
+    else {
+      if(this.apiLoadsDone == this.apiLoadsNeeded) {
+        this.loading.dismiss();
+        //reset for next page load
+        this.apiLoadsDone = 0;
+      }    
+    }
+       
+  }
 }
 
